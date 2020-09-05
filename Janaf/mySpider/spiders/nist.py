e689a7f8ab113ae6df56f0325abda31f9ea05e3e
Landen <Au.Pieces_Landen@foxmail.com>
2020-09-06 02:51:36 +0800

    some test OK!
    Manual checking is recommended.

--- Janaf/mySpider/spiders/nist.py
+++ Janaf/mySpider/spiders/nist.py
@@ -5,12 +5,12 @@ from ..items import Nist
 
 class NistSpider(scrapy.Spider):
     name = 'nist'
-    start_urls = ['https://webbook.nist.gov/cgi/formula/',]
-    #start_urls = ['https://webbook.nist.gov/cgi/formula/Ac',]
+    #start_urls = ['https://webbook.nist.gov/cgi/formula/',]
+    start_urls = ['https://webbook.nist.gov/cgi/formula/Te',]
     prefix="https://webbook.nist.gov"
     output=[]
     next_url=[]
-    path="./dataset/nist.xls"
+    #path="./dataset/nist.xls"
     path4all="./dataset/nist_url.xls"
     #start_urls = ['https://webbook.nist.gov/cgi/formula?ID=C54038016','https://webbook.nist.gov/cgi/inchi/InChI%3D1S/No','https://webbook.nist.gov/cgi/inchi/InChI%3D1S/Ba']
     #start_urls = ['https://webbook.nist.gov/cgi/inchi?ID=C7440393&Mask=20#Ion-Energetics','https://webbook.nist.gov/cgi/inchi?ID=C10028145&Mask=20#Ion-Energetics']
@@ -24,9 +24,10 @@ class NistSpider(scrapy.Spider):
             if element.find("species")!=-1:
                 self.next_url.append(url)
             else:
-                self.output.append([element,url])
-                urls.append([element,url])
-        a2xl(self.path,urls)
+                yield scrapy.Request(url,callback=self.idea_parse)
+                #self.output.append([element,url])
+                #urls.append([element,url])
+        #a2xl(self.path,urls)
         if self.next_url!=[]:
             print("len:",len(self.next_url))
             for i in self.next_url:
@@ -44,20 +45,12 @@ class NistSpider(scrapy.Spider):
                     temp=each.xpath("./a/text()").extract_first()
                     if temp!=None and temp.find("ion energetics")!=-1:
                         #output.append([prefix+each.xpath("./a/@href").extract_first()])
-                        url=prefix+each.xpath("./a/@href").extract_first()
+                        url=self.prefix+each.xpath("./a/@href").extract_first()
                         yield scrapy.Request(url,callback=self.data_parse)
     
     def data_parse(self, response):
         data=[]
         y=Nist()
-        y['Name']=response.xpath('//*[@id="Top"]')
-        items= response.xpath('//*[@id="main"]//ul//li')
-        for no,item in enumerate(items):
-            tmp=item.xpat("./strong/text()").extract_first()
-            if no==0:
-                y['Formula']=item.xpath("./text()")
-            if tmp!=None and tmp.find("CAS")!=-1:
-                y['CAS']=item.xpath("./text()")
         items= response.xpath("//table")
         for no,item in enumerate(items):
             tmp=item.xpath("./@aria-label").extract_first()
@@ -66,6 +59,7 @@ class NistSpider(scrapy.Spider):
                 for nu,each in enumerate(eachs):
                     if nu==1:
                         everys= each.xpath(".//td")
+                        tmp=[]
                         for every in everys:
                             text=every.xpath("./text()").extract_first()
                             if text!=None:
@@ -77,11 +71,25 @@ class NistSpider(scrapy.Spider):
                                 else:
                                     text= every.xpath("./em/text()").extract_first()
                                     tmp.append(text)
-                        y['IE'] = tmp[0]
-                        y['Method']=tmp[1]
-                        y['Refer']=tmp[2]
-                        y['Comment']=tmp[3]
-        data.append([y])
-        a2xl(self.path4all,data)
-        yield y
+                        if len(tmp)>0:
+                            y['IE'] = tmp[0]
+                        if len(tmp)>1:
+                            y['Method']=tmp[1]
+                        if len(tmp)>2:
+                            y['Refer']=tmp[2]
+                        if len(tmp)>3:
+                            y['Comment']=tmp[3]
+                        if y['IE']!="":
+                            y['Name']=response.xpath('//*[@id="Top"]/text()').extract_first()
+                            items= response.xpath('//*[@id="main"]//ul//li')
+                            for no,item in enumerate(items):
+                                tmp=item.xpath("./strong/text()").extract_first()
+                                if no==0:
+                                    y['Formula']=item.xpath("./text()").extract_first()
+                                if tmp!=None and tmp.find("CAS")!=-1:
+                                    y['CAS']=item.xpath("./text()").extract_first()
+                        print("y",y)
+                        data.append([y])
+                        #a2xl(self.path4all,data)
+                        yield y
         
